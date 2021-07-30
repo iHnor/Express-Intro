bdc4f082160a215e5c934d281f07c594579fd801
Ihor Mykhno <igormykhno@gmail.com>
2021-07-30 17:21:18 +0300

    Final reactoring

--- ToDoServer.js
+++ ToDoServer.js
@@ -1,40 +1,51 @@
-import http from 'http'
+import express from 'express'
+const app = express()
 
-function logRequest({ method, url }) {
+function logRequest({ method, url }, res, next) {
     console.log(`[${new Date().toISOString()}] ${method} ${url}`);
+    next()
 }
 
-const tasks = [{ name: 'Get tasks' }, { name: 'Clear task' }]
-const server = http.createServer((req, res) => {
-    logRequest(req)
-    if (req.url === '/tasks') {
-        if (req.method === 'GET') {
-            res.writeHead(200, { 'Count-Type': 'application/json' })
-            res.end(JSON.stringify(tasks))
-        }
-        else if (req.method === 'POST') {
-            const date = []
-            req.on('data', message => date.push(message))
-            req.on('end', () => {
-                const task = JSON.parse(date.push(chunk))
-                task.push(task)
-                req.writeHead(201, { 'Content-Type': 'application/json' })
-                req.end(JSON.stringify(task))
-            })
-        }
-        else {
-            res.writeHead(404, 'Not Found')
-            res.end()
-        }
+app.use(express.json())
+app.use(logRequest)
 
+
+const inc = (index = 0) => () => ++index
+const genId = inc()
+const todoitems = [
+    { id: genId(), name: 'Test task', done: false },
+    { id: genId(), name: 'We create new task', done: false }
+]
+
+const createTask = data => {
+    return {
+        id: genId(),
+        name: data.name,
+        done: false
+    }
+}
+
+app.get('/todoitems', (req, res) => res.json(todoitems))
+
+app.post('/todoitems', (req, res) => {
+    const todoitem = createTask(req.body)
+    todoitems.push(todoitem)
+    res.json(todoitem)
+})
+
+app.patch('/todoitems/:id', (req, res) => {
+    const todoitemId = parseInt(req.params.id)
+    const todoitem = todoitems.find(t => t.id === todoitemId)
+    if (todoitem) {
+        Object.assign(todoitem, req.body)
+        res.json(todoitem)
     }
     else {
-        res.writeHead(404, 'Not Found')
-        res.end()
+        res.status(404).json({ error: 'Task not found' })
     }
 })
 
 const port = 2000;
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server started at localhost: ${port}`);
 })
