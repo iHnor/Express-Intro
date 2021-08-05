2b51b8089f9f006da2071c65f9eabf2f30266be2
Ihor Mykhno <igormykhno@gmail.com>
2021-08-05 18:05:22 +0300

    add link to tasks request

--- routers/index.js
+++ routers/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router();
 
-const task = require('./lists')
+const list = require('./lists')
+const task = require('./tasks')
 
-router
-  .use('/lists', task)
-//   .use('/meetups', meetups)
+router 
+  .use('/lists', list)
+  .use('/tasks', task)
 
 module.exports = router
