23ab6f9c52b565a19ed66920c719edb2dcb21f31
Ihor Mykhno <igormykhno@gmail.com>
2021-08-11 11:17:18 +0300

    Add method Put and tests all functions

--- models/models.js
+++ models/models.js
@@ -6,21 +6,21 @@ class ToDO {
     async findAll() {
         const client = new Client(config);
         client.connect();
-        let sqlComand = await client.query('SELECT text, done, id FROM tasks');
+        let sqlComand = await client.query('SELECT task, done, id FROM tasks');
         return sqlComand.rows;
     }
 
     async findList(listId) {
         const client = new Client(config);
         client.connect();
-        let sqlComand = await client.query('SELECT task, done FROM tasks  RIGHT JOIN "group" ON tasks.list_id = group.id WHERE tasks.list_id = $1', [listId])
+        let sqlComand = await client.query('SELECT task, done, tasks.id FROM tasks  RIGHT JOIN "group" ON tasks.list_id = "group".id WHERE tasks.list_id = $1', [listId])
         return sqlComand.rows;
     }
 
     async findTask(taskId, listId) {
         const client = new Client(config);
         client.connect();
-        let sqlComand = await client.query('SELECT task, done FROM tasks  RIGHT JOIN "group" ON tasks.list_id = groups.id WHERE tasks.list_id = $1', [listId])
+        let sqlComand = await client.query('SELECT task, done, tasks.id FROM tasks  RIGHT JOIN "group" ON tasks.list_id = "group".id WHERE tasks.list_id = $1', [listId])
         return sqlComand.rows[+taskId - 1]
     }
 
@@ -39,28 +39,30 @@ class ToDO {
         await client.query('UPDATE tasks SET task = $1 WHERE id = $2;', [NewUpdateTask['name'], taskId])
     }
 
-    // async changeTask(taskId, listId, newTask) {
-    //     const client = new Client(config);
-    //     client.connect();
-    // }
+    async changeTask(taskId, newTask) {
+        const client = new Client(config);
+        client.connect();
+
+        let task = newTask.task ?? 'Default task'
+        let done = newTask.done ?? false;
+        let list_id = newTask.list_id ?? 0
+        await client.query('UPDATE tasks SET task = $1, done = $2, list_id = $3  WHERE id = $4;', [task, done, +list_id, +taskId])
+        let hasId =  await client.query('SELECT id FROM "group" WHERE id = $1', [+list_id]);
+        if (hasId.rows.length == 0)
+            await client.query('INSERT INTO "group" (id) VALUES ($1) ;', [ +list_id]);
+    }
 
     async deleteTask(taskId) {
         const client = new Client(config);
         client.connect();
-        let hasId =  await client.query('SELECT list_id from tasks WHERE list_id = $1', [+taskId]);
-        console.log(hasId.rows.length);
-        if (hasId.rows.length == 0){
-            let listId =  await client.query('SELECT list_id from tasks WHERE id = $1;', [+taskId])
-            console.log(listId);
-            // await client.query('DELETE FROM "group" WHERE id = $1;', [+listId])
-        }
         await client.query('DELETE FROM tasks WHERE id = $1;', [+taskId])
     }
+
     async deleteList(listId){
         const client = new Client(config);
         client.connect();
         await client.query('DELETE FROM tasks WHERE list_id = $1;', [listId])
-        await client.query('DELETE FROM "group" WHERE id = $1;;', [listId])  
+        await client.query('DELETE FROM "group" WHERE id = $1;', [listId])  
     }
 }
 
